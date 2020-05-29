bf8d2febeddd343eeccd31c1c195bde2a3200382
Sheeks Bhattacharjee <35619776+Sheeks-Bhattacharjee@users.noreply.github.com>
2020-05-28 21:11:01 -0400

    Update Pellet.java

--- Pellet.java
+++ Pellet.java
@@ -2,28 +2,29 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.geom.*;
 
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 public class Pellet extends JComponent
 {
-	private Ellipse2D.Double pellet = new Ellipse2D.Double(0,0,14,14);
 	
 	public Pellet(int x, int y) 
 	{
-		this.setSize(new Dimension(15,15));
+		this.setSize(new Dimension(21,41));
 		this.setLocation(x,y);
 	}
 	
 	public void paintComponent(Graphics g)
 	{
-		Graphics2D g2 = (Graphics2D) g;
-		
-		g2.setColor(Color.WHITE);
-		g2.fill(pellet);
-		g2.setColor(Color.WHITE);
-		g2.draw(pellet);
+		super.paintComponent(g);
+		g.setColor(Color.RED);
+		g.fillOval(0, 20, 20, 17);
+		g.setColor(Color.black);
+		g.fillRect(10, 15, 3, 7);
+		g.setColor(Color.green);
+		g.fillRect(5, 13, 5, 3);
 	}
-	
 
 }
