8d8f7ad549067335228a2950769cc0460f2efa3f
Sheeks Bhattacharjee <35619776+Sheeks-Bhattacharjee@users.noreply.github.com>
2020-06-04 20:23:41 -0400

    Update BadPellet.java

--- BadPellet.java
+++ BadPellet.java
@@ -8,21 +8,41 @@ import javax.swing.JComponent;
 public class BadPellet extends JComponent
 {
 	
+	private Rectangle borderBad;
+	
 	public BadPellet(int x, int y) 
 	{
-		this.setSize(new Dimension(21,41));
+		this.setSize(new Dimension(25,45));
 		this.setLocation(x,y);
+
+		borderBad = new Rectangle (x, y,21,41);
+	}
+	
+	public Rectangle returnBorder()
+	{
+		return borderBad;
+	}
+	
+	private void setBorder(int x, int y)
+	{
+		borderBad.setLocation(x,y);
 	}
 	
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		g.setColor(new Color(65,0,77,1));
+		g.setColor(new Color(99, 15, 255,255));
 		g.fillOval(0, 20, 20, 17);
-		g.setColor(Color.black);
+		g.setColor(Color.BLACK);
 		g.fillRect(10, 15, 3, 7);
 		g.setColor(Color.DARK_GRAY);
 		g.fillRect(5, 13, 5, 3);
 	}
+	
+	public void update()
+	{
+		this.setBorder(this.getX(), this.getY());
+	}
+
 
 }
