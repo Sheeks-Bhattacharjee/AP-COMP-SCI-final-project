cca1c1e9a0f9cbbd3cf5f3f506e2d540ba906ce5
Sheeks Bhattacharjee <35619776+Sheeks-Bhattacharjee@users.noreply.github.com>
2020-06-04 20:24:46 -0400

    Update Pellet.java

--- Pellet.java
+++ Pellet.java
@@ -1,19 +1,30 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.geom.*;
+import java.awt.Rectangle;
 
-import javax.swing.ImageIcon;
 import javax.swing.JComponent;
+import javax.swing.border.Border;
+
 public class Pellet extends JComponent
 {
-	
+	private Rectangle borderBall;
 	public Pellet(int x, int y) 
 	{
 		this.setSize(new Dimension(21,41));
 		this.setLocation(x,y);
+		
+		borderBall = new Rectangle (x,y,21,41);
+	}
+	
+	public Rectangle returnBorder()
+	{
+		return borderBall;
+	}
+	
+	private void setBorder(int x, int y)
+	{
+		borderBall.setLocation(x, y);
 	}
 	
 	public void paintComponent(Graphics g)
@@ -25,6 +36,11 @@ public class Pellet extends JComponent
 		g.fillRect(10, 15, 3, 7);
 		g.setColor(Color.green);
 		g.fillRect(5, 13, 5, 3);
+	}
+	
+	public void update()
+	{
+		this.setBorder(this.getX(), this.getY());
 	}
 
 }
