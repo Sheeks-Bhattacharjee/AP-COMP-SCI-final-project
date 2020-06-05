0cd49d708fecdf6a8217a4be5c44f5dba37da1ae
Sheeks Bhattacharjee <35619776+Sheeks-Bhattacharjee@users.noreply.github.com>
2020-06-04 20:25:14 -0400

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -10,12 +10,26 @@ import javax.swing.JComponent;
 public class Snake extends JComponent
 {
 	private int dx, dy;
-	private Ellipse2D.Double snek = new Ellipse2D.Double(0,0,14,14); 
+	private Ellipse2D.Double snek = new Ellipse2D.Double(0,0,20,20);
+	private Rectangle borderSnek;
 	public Snake(int x, int y) 
 	{
-		this.setSize(new Dimension(15,15));
+		this.setSize(new Dimension(21,21));
 		this.setLocation(x,y);
+		
+		borderSnek = new Rectangle (x,y,20,20);
 	}
+	
+	public Rectangle returnBorder()
+	{
+		return borderSnek;
+	}
+	
+	private void setBorder(int x, int y)
+	{
+		borderSnek.setLocation(x, y);
+	}
+	
 	public void setDx(int dx)
 	{
 		this.dx = dx;
@@ -29,13 +43,14 @@ public class Snake extends JComponent
 	public void update()
 	{
 		this.setLocation(this.getX() + dx, this.getY() + dy);
+		this.setBorder(this.getX(), this.getY());
 	}
 	
 	public void paintComponent(Graphics g)
 	{
 		Graphics2D g2 = (Graphics2D) g;
 		
-		g2.setColor(Color.BLACK);				//new Color(210,255,53,255));
+		g2.setColor(new Color(10, 255, 55, 255));				//new Color(210,255,53,255));
 		g2.fill(snek);
 		
 		g2.setColor(Color.BLACK);
